f2dd3a4a68180449c4573cc6c287551a543aca2e
ciropiccolo <ciro.piccolo-cic@ibm.com>
2025-07-22 00:17:55 +0200

    allowed hosts

--- src/routes/api/files/+server.ts
+++ src/routes/api/files/+server.ts
@@ -1,7 +1,9 @@
-import { prisma } from '$lib/db';
-import type { RequestHandler } from '@sveltejs/kit';
+import { prisma } from "$lib/db";
+import type { RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async () => {
-  const files = await prisma.upload.findMany({ orderBy: { createdAt: 'desc' } });
+  const files = await prisma.upload.findMany({
+    orderBy: { createdat: "desc" },
+  });
   return new Response(JSON.stringify(files));
 };
